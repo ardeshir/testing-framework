5af712fde4283ea4ca8593b0f75b3a992c88b345
ardeshir sepahsalar <ardeshir.org@gmail.com>
2018-11-02 21:10:34 -0500

    adding assertion library

--- app.js
+++ app.js
@@ -6,19 +6,40 @@ const {sum, sub} = require('./math')
 let result   = sum(3, 7)
 let expected = 10
 
-if (result !== expected) {
+expect(result).toBe(expected)
+
+/* if (result !== expected) {
  throw new Error(`${result} is not equal to ${expected}`)
 
 } else {
  console.log("sum() expected: " + expected + " == result: " + result)
 }
+*/
 
 result   = sub(7, 3)
 expected = 4
 
+expect(result).toBe(expected)
+
+/* NICE ABSTRCTION */ 
+
+/* 
 if (result !== expected) {
  throw new Error(`${result} is not equal to ${expected}`)
 
 } else {
  console.log("sub() expected: " + expected  + " == result: " + result)
+}
+*/
+
+// expect(resutl).toBe(expected) 
+
+function expect(actual) {
+  return {
+     toBe(expected) {
+        if (actual !== expected) {
+          throw new Error(`${actual} is not equal to ${expected}`)
+        }
+     }
+  }
 }
